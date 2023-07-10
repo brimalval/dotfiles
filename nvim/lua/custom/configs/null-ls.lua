5d11ff1d4c2dd722ae761180579f6e16e905a414
brimalval <brianmalcolm.v@gmail.com>
2023-07-10 23:06:56 +0800

    add custom folder

---
+++ nvim/lua/custom/configs/null-ls.lua
@@ -0,0 +1,41 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls = require('null-ls')
+
+local python_executable = io.popen('which python'):read('*line')
+print("Python Executable:", python_executable)
+
+local sources = {
+  null_ls.builtins.formatting.black.with({
+    extra_args = { "-l", "80" },
+  }),
+  null_ls.builtins.diagnostics.flake8,
+  null_ls.builtins.diagnostics.mypy.with({
+    extra_args = function(params)
+        return python_executable and {
+            "--python-executable",
+            python_executable,
+        }
+    end,
+  }),
+  null_ls.builtins.diagnostics.ruff,
+}
+
+local opts = {
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+}
+return opts
