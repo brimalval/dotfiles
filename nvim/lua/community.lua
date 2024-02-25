da3f39c9f07818cb4303fd98a034c140ff8a6a90
brimalval <brianmalcolm.v@gmail.com>
2024-02-25 20:27:52 +0800

    AstroNvim v4 add important language support

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -7,8 +7,17 @@
 ---@type LazySpec
 return {
   -- TODO: Remove branch v4 on release
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  {
+    "AstroNvim/astrocommunity",
+    branch = "v4",
+  },
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
-  -- { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
 }
--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -12,21 +12,21 @@ return {
     -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 100, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      autopairs = true,                                 -- enable autopairs at start
+      cmp = true,                                       -- enable completion at start
+      highlighturl = true,                              -- highlight URLs at start
+      notifications = true,                             -- enable notifications at start
     },
     -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
+      opt = {                  -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-        wrap = false, -- sets vim.opt.wrap
+        number = true,         -- sets vim.opt.number
+        spell = false,         -- sets vim.opt.spell
+        signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
+        wrap = false,          -- sets vim.opt.wrap
       },
-      g = { -- vim.g.<key>
+      g = {                    -- vim.g.<key>
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
@@ -66,6 +66,11 @@ return {
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
         ["<Leader>G"] = { "<Cmd>Git<CR>", desc = "Open Git fugitive" },
         ["<leader>sf"] = { "<cmd> noa w <CR>", desc = "Save without formatting" },
+        ["<leader>td"] = { "<Cmd> TroubleToggle document_diagnostics <CR>", desc = "Get current document diagnostics" },
+        ["<leader>tD"] = {
+          "<Cmd> TroubleToggle workspace_diagnostics <CR>",
+          desc = "Get current workspace diagnostics",
+        },
       },
       t = {
         -- setting a mapping to false will disable it
