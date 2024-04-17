851711263b1ccd4780358492b6fccda5445eefd6
brimalval <brianmalcolm.v@gmail.com>
2024-04-17 13:17:48 +0800

    AstroNvim v4 release

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -6,10 +6,8 @@
 
 ---@type LazySpec
 return {
-  -- TODO: Remove branch v4 on release
   {
     "AstroNvim/astrocommunity",
-    branch = "v4",
   },
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
