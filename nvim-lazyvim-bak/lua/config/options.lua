63afc0e708b64366a2fbe33c1cdd8d698d19077c
brimalval <brianmalcolm.v@gmail.com>
2026-03-10 14:41:23 +0800

    chore: rm nvim backups

--- nvim-lazyvim-bak/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-local opt = vim.opt
-
-opt.showtabline = 0
--- Constrain the text width
-opt.wrap = true
